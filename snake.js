448d35c2d7ad7cec7fb808026603cd9b2911d6ff
Aviralpandeycse <aviralpandey003@gmail.com>
2020-07-30 23:55:36 +0530

    created simple snake of length 5 moving only right.

--- snake.js
+++ snake.js
@@ -2,5 +2,53 @@ function init() {
   var canvas = document.getElementById("mycanvas");
   W = H = canvas.width = canvas.height = 1000;
   pen = canvas.getContext("2d");
+  cs = 66;
+  snake = {
+    init_len: 5,
+    color: "blue",
+    cells: [],
+    direction: "right",
+    createSnake: function () {
+      for (var i = 0; i < this.init_len; i++) {
+        this.cells.push({ x: i, y: 0 });
+      }
+    },
+    drawSnake: function () {
+      for (var i = 0; i < this.cells.length; i++) {
+        pen.fillStyle = this.color;
+        pen.fillRect(
+          this.cells[i].x * cs,
+          this.cells[i].y * cs,
+          cs - 2,
+          cs - 2
+        );
+      }
+    },
+    updateSnake: function () {
+      this.cells.shift();
+      var headX = this.cells[this.cells.length - 1].x;
+      var headY = this.cells[this.cells.length - 1].y;
+      var X = headX + 1;
+      var Y = headY;
+      this.cells.push({ x: X, y: Y });
+    },
+  };
+  snake.createSnake();
 }
+
+function draw() {
+  pen.clearRect(0, 0, W, H);
+  snake.drawSnake();
+}
+
+function update() {
+  snake.updateSnake();
+}
+
+function gameloop() {
+  draw();
+  update();
+}
+
 init();
+var f = setInterval(gameloop, 100);
